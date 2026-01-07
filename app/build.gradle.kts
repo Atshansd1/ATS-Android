0e2261d5a40b12a6718da757753311e7c0b861d1
Mohanad <mohanadsd@example.com>
2026-01-07 19:42:52 +0400

    Bump version to 2.2.2 with update feature

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.ats.android"
         minSdk = 26
         targetSdk = 34
-        versionCode = 50
-        versionName = "2.2.1"
+        versionCode = 51
+        versionName = "2.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
